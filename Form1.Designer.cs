795837defbf7bf81417645d576a34d9a881ac1d6
buzzibaer <buzzibaer@bust.name>
2023-11-25 15:10:27 +0100

    added version 2 and version 3 fan differences to the gui

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -43,10 +43,27 @@ namespace FanPlugin.Test
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.button9 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.groupBox6.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
@@ -61,11 +78,11 @@ namespace FanPlugin.Test
             // 
             // tbLog
             // 
-            this.tbLog.Location = new System.Drawing.Point(48, 226);
+            this.tbLog.Location = new System.Drawing.Point(22, 319);
             this.tbLog.Multiline = true;
             this.tbLog.Name = "tbLog";
             this.tbLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.tbLog.Size = new System.Drawing.Size(640, 120);
+            this.tbLog.Size = new System.Drawing.Size(1141, 212);
             this.tbLog.TabIndex = 1;
             // 
             // textBox1
@@ -75,6 +92,7 @@ namespace FanPlugin.Test
             this.textBox1.Size = new System.Drawing.Size(37, 20);
             this.textBox1.TabIndex = 2;
             this.textBox1.Text = "6";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label1
             // 
@@ -89,7 +107,7 @@ namespace FanPlugin.Test
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(45, 210);
+            this.label2.Location = new System.Drawing.Point(19, 303);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(143, 13);
             this.label2.TabIndex = 5;
@@ -97,7 +115,7 @@ namespace FanPlugin.Test
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(77, 129);
+            this.button3.Location = new System.Drawing.Point(55, 129);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(153, 23);
             this.button3.TabIndex = 6;
@@ -107,7 +125,7 @@ namespace FanPlugin.Test
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(77, 158);
+            this.button4.Location = new System.Drawing.Point(55, 158);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(153, 23);
             this.button4.TabIndex = 7;
@@ -117,7 +135,7 @@ namespace FanPlugin.Test
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(535, 175);
+            this.button5.Location = new System.Drawing.Point(55, 227);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(153, 23);
             this.button5.TabIndex = 8;
@@ -141,7 +159,7 @@ namespace FanPlugin.Test
             this.groupBox1.Controls.Add(this.textBox2);
             this.groupBox1.Controls.Add(this.radioButton2);
             this.groupBox1.Controls.Add(this.radioButton1);
-            this.groupBox1.Location = new System.Drawing.Point(269, 23);
+            this.groupBox1.Location = new System.Drawing.Point(247, 23);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(264, 197);
             this.groupBox1.TabIndex = 11;
@@ -175,47 +193,188 @@ namespace FanPlugin.Test
             this.groupBox2.Controls.Add(this.button1);
             this.groupBox2.Controls.Add(this.textBox1);
             this.groupBox2.Controls.Add(this.label1);
-            this.groupBox2.Location = new System.Drawing.Point(41, 23);
+            this.groupBox2.Location = new System.Drawing.Point(19, 23);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(200, 100);
             this.groupBox2.TabIndex = 12;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Video Selection";
             // 
-            // button2
+            // groupBox3
             // 
-            this.button2.Location = new System.Drawing.Point(539, 36);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(153, 23);
-            this.button2.TabIndex = 13;
-            this.button2.Text = "Turn Fan ON";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            this.groupBox3.Controls.Add(this.groupBox2);
+            this.groupBox3.Controls.Add(this.groupBox1);
+            this.groupBox3.Controls.Add(this.button5);
+            this.groupBox3.Controls.Add(this.button4);
+            this.groupBox3.Controls.Add(this.button3);
+            this.groupBox3.Location = new System.Drawing.Point(22, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(543, 270);
+            this.groupBox3.TabIndex = 15;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "LED Fan Version 2";
             // 
-            // button6
+            // groupBox4
             // 
-            this.button6.Location = new System.Drawing.Point(539, 65);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(153, 23);
-            this.button6.TabIndex = 14;
-            this.button6.Text = "Turn Fan OFF";
-            this.button6.UseVisualStyleBackColor = true;
-            this.button6.Click += new System.EventHandler(this.button6_Click_1);
+            this.groupBox4.Controls.Add(this.button7);
+            this.groupBox4.Controls.Add(this.button8);
+            this.groupBox4.Controls.Add(this.groupBox5);
+            this.groupBox4.Controls.Add(this.groupBox6);
+            this.groupBox4.Controls.Add(this.button10);
+            this.groupBox4.Controls.Add(this.button11);
+            this.groupBox4.Controls.Add(this.button12);
+            this.groupBox4.Location = new System.Drawing.Point(582, 12);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(581, 270);
+            this.groupBox4.TabIndex = 17;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "LED Fan Version 3";
+            this.groupBox4.Enter += new System.EventHandler(this.groupBox4_Enter);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(404, 226);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(153, 23);
+            this.button7.TabIndex = 14;
+            this.button7.Text = "Turn Fan OFF";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(245, 226);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(153, 23);
+            this.button8.TabIndex = 13;
+            this.button8.Text = "Turn Fan ON";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.button9);
+            this.groupBox5.Controls.Add(this.textBox3);
+            this.groupBox5.Controls.Add(this.label3);
+            this.groupBox5.Location = new System.Drawing.Point(19, 23);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(200, 100);
+            this.groupBox5.TabIndex = 12;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Video Selection";
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(36, 56);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(153, 23);
+            this.button9.TabIndex = 0;
+            this.button9.Text = "Set Video on Fan";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(152, 30);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(37, 20);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.Text = "6";
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Select VideoId for Fan";
+            // 
+            // groupBox6
+            // 
+            this.groupBox6.Controls.Add(this.textBox4);
+            this.groupBox6.Controls.Add(this.radioButton3);
+            this.groupBox6.Controls.Add(this.radioButton4);
+            this.groupBox6.Location = new System.Drawing.Point(247, 23);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Size = new System.Drawing.Size(264, 197);
+            this.groupBox6.TabIndex = 11;
+            this.groupBox6.TabStop = false;
+            this.groupBox6.Text = "Video Options";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(15, 75);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(229, 100);
+            this.textBox4.TabIndex = 13;
+            this.textBox4.Text = resources.GetString("textBox4.Text");
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(15, 39);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(136, 17);
+            this.radioButton3.TabIndex = 11;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Loop through all Videos";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(15, 19);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(150, 17);
+            this.radioButton4.TabIndex = 10;
+            this.radioButton4.Text = "Stop Cycle through Videos";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(55, 227);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(153, 23);
+            this.button10.TabIndex = 8;
+            this.button10.Text = "Play Old Video File";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(55, 158);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(153, 23);
+            this.button11.TabIndex = 7;
+            this.button11.Text = "<< Play last Video";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(55, 129);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(153, 23);
+            this.button12.TabIndex = 6;
+            this.button12.Text = "Get File List From Fan";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(737, 393);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.groupBox2);
-            this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.label2);
+            this.ClientSize = new System.Drawing.Size(1191, 556);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.tbLog);
+            this.Controls.Add(this.label2);
             this.Name = "Form1";
             this.Text = "Fan Test App";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -223,6 +382,12 @@ namespace FanPlugin.Test
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
+            this.groupBox6.ResumeLayout(false);
+            this.groupBox6.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -243,8 +408,21 @@ namespace FanPlugin.Test
         private System.Windows.Forms.RadioButton radioButton2;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox6;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -112,22 +112,102 @@ namespace FanPlugin.Test
             }
         }
 
-    
-
-        private void button2_Click_1(object sender, EventArgs e)
+        private void groupBox4_Enter(object sender, EventArgs e)
         {
-            FanPlugin.Wrapper.Fan fan = new Wrapper.Fan();
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            FanPlugin.Wrapper.FanV3 fan = new Wrapper.FanV3();
+
+            tbLog.Clear();
+            tbLog.AppendText(fan.playVideoWithId(textBox1.Text));
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            FanPlugin.Wrapper.FanV3 fan = new Wrapper.FanV3();
+
+            tbLog.Clear();
+            tbLog.AppendText(fan.getFileListFromFan());
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            FanPlugin.Wrapper.FanV3 fan = new Wrapper.FanV3();
+
+            tbLog.Clear();
+            tbLog.AppendText(fan.playLastFromFan());
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            // Check of the raiser of the event is a checked Checkbox.
+            // Of course we also need to to cast it first.
+            if (((RadioButton)sender).Checked)
+            {
+                // This is the correct control.
+                RadioButton rb = (RadioButton)sender;
+
+                FanPlugin.Wrapper.FanV3 fan = new Wrapper.FanV3();
+
+                tbLog.Clear();
+                tbLog.AppendText(fan.selectSingleVideoPlaybackMode());
+                tbLog.AppendText("RadioButton Single Video Mode Selected");
+            }
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            // Check of the raiser of the event is a checked Checkbox.
+            // Of course we also need to to cast it first.
+            if (((RadioButton)sender).Checked)
+            {
+                // This is the correct control.
+                RadioButton rb = (RadioButton)sender;
+
+
+                FanPlugin.Wrapper.FanV3 fan = new Wrapper.FanV3();
+
+                tbLog.Clear();
+                tbLog.AppendText(fan.selectLoopVideoPlaybackMode());
+                tbLog.AppendText("RadioButton Loop Video Mode Selected");
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            FanPlugin.Wrapper.FanV3 fan = new Wrapper.FanV3();
 
             tbLog.Clear();
             tbLog.AppendText(fan.sendPowerOn());
         }
 
-        private void button6_Click_1(object sender, EventArgs e)
+        private void button7_Click(object sender, EventArgs e)
         {
-            FanPlugin.Wrapper.Fan fan = new Wrapper.Fan();
+            FanPlugin.Wrapper.FanV3 fan = new Wrapper.FanV3();
 
             tbLog.Clear();
             tbLog.AppendText(fan.sendPowerOff());
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            FanPlugin.Wrapper.FanV3 fan = new Wrapper.FanV3();
+
+            tbLog.Clear();
+            tbLog.AppendText(fan.playOldFromFan());
         }
     }
 }
